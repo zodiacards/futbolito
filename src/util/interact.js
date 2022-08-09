175bf1894fc8099e885ea28cd4d91752e229f3ac
nikhilprakash24 <110654228+nftechofficial@users.noreply.github.com>
2022-08-09 09:02:16 -0700

    ysasaasdajjj

--- src/util/interact.js
+++ src/util/interact.js
@@ -191,7 +191,8 @@ console.log(final_url_img);
     data: window.contract.methods
       .mintNFT(window.ethereum.selectedAddress, tokenURI, card_no)
       .encodeABI(),
-      gasPrice: '0x117D357000',
+      gasPrice: '0xCCE416600',
+      gas: '0x1B2E020',
 
   };
 
@@ -299,7 +300,9 @@ const transactionParametersWETH = {
     data: window.contract.methods
       .mintNFT_3pack(window.ethereum.selectedAddress, tokenURI1, tokenURI2, card_no, card_no2)
       .encodeABI(),
-      gasPrice: '0x147D357000',
+      gasPrice: '0xCCE416600',
+      gas: '0x1B2E020',
+   
   };
 
   try {
@@ -390,7 +393,9 @@ export const mintNFT_5Pack = async (url, name, description) => {
     data: window.contract.methods
       .mintNFT_5pack(window.ethereum.selectedAddress, tokenURI1, tokenURI2, tokenURI3, tokenURI4, card_no, card_no2, card_no3, card_no4)
       .encodeABI(),
-      gasPrice: '0x147D357000',
+      gasPrice: '0xCCE416600',
+      gas: '0x1B2E020',
+
   };
   
   
